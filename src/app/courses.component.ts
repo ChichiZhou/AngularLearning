2179d63efb88201a08d3abbea93f670c11b671c2
He Zhou <hezhou@HedeMacBook-Pro.lan>
2020-07-20 14:23:16 -0500

    Create CourseService

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,5 +1,6 @@
 // Use export so Angular can see this component
 import { Component } from '@angular/core';
+import { CoursesService } from './courses.service';
 @Component({
     selector: 'courses',
     // 这里使用 `` 可以将 template 分成许多行，这样便于阅读
@@ -19,6 +20,16 @@ export class CoursesComponent{
     getTitle(){
         return this.title;
     };
-    courses = ["course1", "course2", "course3"];
+    courses;
+
+    // Dependency injection
+    constructor(service: CoursesService){
+        this.courses = service.getCourses();
+    }
+    // If use logic for calling an HTTP service, there will be the following cons
+    // 1.This will depend on the HTTP endpoints. It's not a good choice for testing
+    // 2.This is not extenable.
+    // 3.The component should not include others besides the presentation logic.
+    
 
 }
---
+++ src/app/courses.service.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+export class CoursesService{
+    getCourses(){
+        return ["course1", "course2", "course3"];
+    }
+}
