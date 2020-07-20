23fbb3e7896f3cfa32c4102abd17d3cb1e8a0281
He Zhou <hezhou@HedeMacBook-Pro.lan>
2020-07-20 13:43:35 -0500

    Create a component

---
+++ src/app/courses.component.ts
@@ -0,0 +1,9 @@
+// Use export so Angular can see this component
+import { Component } from '@angular/core';
+@Component({
+    selector: 'courses',
+    template: '<h2>Courses</h2>'
+})
+export class CoursesComponent{
+
+}
