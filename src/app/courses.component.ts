8dd462eb072ba0159eb5de9ad6564142601c027d
He Zhou <hezhou@HedeMacBook-Pro.lan>
2020-07-20 14:06:45 -0500

    Modify the template of courses

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,8 +2,23 @@
 import { Component } from '@angular/core';
 @Component({
     selector: 'courses',
-    template: '<h2>Courses</h2>'
+    // 这里使用 `` 可以将 template 分成许多行，这样便于阅读
+    // Use Directives manipulate the DOM elements
+    template: `
+    <h2>{{ "Title: " + getTitle() }}</h2>
+    <ul>
+        <li *ngFor="let courseElement of courses">
+            {{ courseElement }}
+        </li>
+    </ul>
+    
+    `
 })
 export class CoursesComponent{
+    title = "List of Courses";
+    getTitle(){
+        return this.title;
+    };
+    courses = ["course1", "course2", "course3"];
 
 }
