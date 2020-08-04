1dd623356b41812cd338fde4dbdc25fbc52c7f1c
He Zhou <hezhou@HedeMacBook-Pro.lan>
2020-08-04 10:42:28 -0500

    Add comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
   // title = 'hello-world';
-  title = 'Angular Hello-World';
-  courses = [1,2];
+  title = "Angular Hello-World";
+  courses = [1, 2];
   post = {
     title: "Title",
-    isFavorite: true
+    isFavorite: true,
   };
 
   // 如果点击 app-favorite， 则会执行下面这个方法
-  onFavoriteChanged(eventArgs){
+  // 这个方法是被 click 激活的，是从 component 传出来的
+  onFavoriteChanged(eventArgs) {
     console.log("Favorite Changed: " + eventArgs);
-
   }
 }
--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -8,22 +8,24 @@ import { EventEmitter } from "@angular/core"; // import EventEmitter
 })
 
 // In order to make the component re-usable, we need to add input and output properties
-// Input is used to pass State to the component
-// Output is used to read Events from the component
+// Input is used to pass State to the component 可以从外界给 component 内部的值进行赋值
+// Output is used to read Events from the component component 可以 raise event 到外界
 // Input and Output make the public API of the component
 // Component 的 public API 指的是，其它 component 能够对这个 component 进行某些操作
-// 具体做法是将一些 property 设置成 input property
-// 虽然 DOM 不包括这些 property，但是通过自己的改写，能够让其实现这个操作
 export class FavoriteComponent {
   // 这里用到了 alias，因为当你改变 component 的时候，template 没有改变
   @Input("is-favorite") isFavorite = false; // 这样可以将 isFavorite 设定成能从外界赋值的状态
+
   // 这里用到了 alias，因为当你改变 component 的时候，template 没有改变
+  // 这里声明了一个发射事件的对象
   @Output("changeThisStar") changeThisStar = new EventEmitter();
 
   title: string;
 
   onClick() {
     this.isFavorite = !this.isFavorite;
-    this.changeThisStar.emit({ newValue: this.isFavorite });
+    // raise the event
+    // 可以传递一个值，这个值会传给所以 subscriber of this event
+    this.changeThisStar.emit("FUCK USA!!!!!!!" + this.isFavorite);
   }
 }
